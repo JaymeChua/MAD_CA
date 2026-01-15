9e1bc11eb1d20765b8eed66052fe8077768011b8
Jayme-Chua <95556211+JaymeChua@users.noreply.github.com>
2026-01-15 17:41:55 +0800

    Fix: Call AppNavigation within setContent to render UI

--- Basic/app/src/main/java/np/mad/ca/whackamolebasic/MainActivity.kt
+++ Basic/app/src/main/java/np/mad/ca/whackamolebasic/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WhackAMoleBasicTheme {
+                AppNavigation()
             }
         }
     }
