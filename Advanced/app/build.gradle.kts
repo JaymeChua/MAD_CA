991eaaf242e797e6a377d618675776f949ad24ee
Jayme-Chua <95556211+JaymeChua@users.noreply.github.com>
2026-01-19 22:07:14 +0800

    Revert from ksp to kapt

--- Advanced/app/build.gradle.kts
+++ Advanced/app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("com.google.devtools.ksp")
+    id("kotlin-kapt")
 }
 
 android {
@@ -63,5 +63,5 @@ dependencies {
     val roomVersion = "2.6.1"
     implementation("androidx.room:room-runtime:$roomVersion")
     implementation("androidx.room:room-ktx:$roomVersion")
-    ksp("androidx.room:room-compiler:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
 }
