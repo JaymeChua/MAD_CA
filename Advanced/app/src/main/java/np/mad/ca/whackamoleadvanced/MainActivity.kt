335414e4cdddd3b62878d80cc30fb351e253fb1b
Jayme-Chua <95556211+JaymeChua@users.noreply.github.com>
2026-01-19 22:08:04 +0800

    Update MainActivity.kt

--- Advanced/app/src/main/java/np/mad/ca/whackamoleadvanced/MainActivity.kt
+++ Advanced/app/src/main/java/np/mad/ca/whackamoleadvanced/MainActivity.kt
@@ -3,40 +3,18 @@ package np.mad.ca.whackamoleadvanced
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import androidx.room.Room
 import np.mad.ca.whackamoleadvanced.ui.theme.WhackAMoleAdvancedTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        val db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "game-db").build()
+        val dao = db.gameDao()
         setContent {
             WhackAMoleAdvancedTheme {
-                AppNavigation()
+                AppNavigationAdvanced(dao)
             }
-        }
-    }
-}
-@Preview
-@Composable
-fun AppNavigation() {
-    val navController = rememberNavController()
-    // Define navigation routes
-    NavHost(navController = navController, startDestination = "game_screen") {
-        composable("game_screen") {
-            GameScreen(
-                onNavigateToSettings = { navController.navigate("settings_screen") }
-            )
-        }
-        composable("settings_screen") {
-            SettingsScreen(
-                onBack = { navController.popBackStack() }
-            )
         }
     }
 }
